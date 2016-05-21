ded705c7562a241b9ff1a8fa8fd1147c55cedb5f
tessahm <tessahm@umich.edu>
2016-05-21 01:09:29 -0500

    YAAAAASSSS go team we did it. Speech -> text and text -> speech. dang.

--- bullseyebuddy.py
+++ bullseyebuddy.py
@@ -8,25 +8,24 @@ from pydub import AudioSegment
 from watson_developer_cloud import SpeechToTextV1, TextToSpeechV1
 
 
-# speech_to_text = SpeechToTextV1(
-#     username='26664ba5-62a0-4e3b-b070-816fa4a67837',
-#     password='kw44VBtY0T3P')
+speech_to_text = SpeechToTextV1(
+    username='26664ba5-62a0-4e3b-b070-816fa4a67837',
+    password='kw44VBtY0T3P')
 
-# print(json.dumps(speech_to_text.models(), indent=2))
+print(json.dumps(speech_to_text.models(), indent=2))
 
-# sound = AudioSegment.from_file('.wma')
+# sound = AudioSegment.from_file('cheese_pizza.wma')
 # sound.export('test.wav', format='wav')
 
-# with open('test.wav', 'rb') as audio_file:
-# 	result = speech_to_text.recognize(audio_file, content_type='audio/wav')
-# 	print(result)    
+with open('cheese_pizza.wav', 'rb') as audio_file:
+	result = speech_to_text.recognize(audio_file, content_type='audio/wav')
+	print(result)    
 
-# 	# TODO: Fix harcoding to access translated text 
-# 	text = result['results'][0]['alternatives'][0]['transcript'].strip()
-# 	print(text)
-text  =  'french%20toast%20cinnamon'
+	# TODO: Fix harcoding to access translated text 
+	text = result['results'][0]['alternatives'][0]['transcript'].strip()
+	print(text)
 
-
+text = '%20'.join(text.split())
 
 url_var = "http://www.target.com/s?category=0|All|matchallpartial|all+categories&searchTerm=%s" % text
 
@@ -54,21 +53,22 @@ response = urllib2.urlopen(url)
 data = json.loads(response.read())
 a= data[u'product_composite_response'][u'items'][0]
 
+speech =  "Wuhf Bark. Item %s is located at aisle %s in block %s floor %s" % (a['general_description'], a['in_store_location'][0]['aisle'], a['in_store_location'][0]['block'], a['in_store_location'][0]['floor'])
 
-print "item %s is located at aisle %s in block %s floor %s" % (a['general_description'], a['in_store_location'][0]['aisle'], a['in_store_location'][0]['block'], a['in_store_location'][0]['floor'])
+print speech
 
-# ALPHABET = string.ascii_lowercase
+ALPHABET = string.ascii_lowercase
 
-# text_to_speech = TextToSpeechV1(
-#     username='c4fb92e9-8a5b-4b6a-9c7b-338ca22b9aae',
-#     password='QZ7UpoSxwb4j')
+text_to_speech = TextToSpeechV1(
+    username='c4fb92e9-8a5b-4b6a-9c7b-338ca22b9aae',
+    password='QZ7UpoSxwb4j')
 
-# print(json.dumps(text_to_speech.voices(), indent=2))
+print(json.dumps(text_to_speech.voices(), indent=2))
 
-# def random_word(length):
-#    return 'bullseye_output_' + \
-#    		''.join(random.choice(string.ascii_lowercase) for i in range(length))
+def random_word(length):
+   return 'bullseye_output_' + \
+   		''.join(random.choice(string.ascii_lowercase) for i in range(length))
 
-# with open('{0}.wav'.format(random_word(4)), 'wb') as audio_file:
-#     audio_file.write(text_to_speech.synthesize(
-#     	'Wuhf Bark. Lucky Charms is on Aisle 3', accept='audio/wav', voice='en-US_AllisonVoice'))
+with open('{0}.wav'.format(random_word(4)), 'wb') as audio_file:
+    audio_file.write(text_to_speech.synthesize(
+    	speech, accept='audio/wav', voice='en-US_AllisonVoice'))
